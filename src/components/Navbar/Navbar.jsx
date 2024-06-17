77d40521e8a0779ad25b7578971f24c34d2b83cb
Dhilip S <139439672+Dhilip546@users.noreply.github.com>
2024-06-17 23:55:28 +0530

    Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -81,7 +81,8 @@ function NavBar() {
               >
                 <AiOutlineContacts style={{ marginBottom: "2px" }} /> Contact Me
               </Nav.Link>
-            </Nav.Item>
+            </Nav.Item+
+            
           </Nav>
         </Navbar.Collapse>
       </Container>
