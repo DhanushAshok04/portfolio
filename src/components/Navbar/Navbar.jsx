26a9ee3f197eb26cba5485146e88def3e11dd549
Dhilip S <139439672+Dhilip546@users.noreply.github.com>
2024-09-15 22:14:40 +0530

    Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -65,7 +65,7 @@ function NavBar() {
             <Nav.Item>
               {/* Use anchor tag with href to Google Drive link */}
               <Nav.Link
-                href="https://drive.google.com/file/d/1wVEXXd1nZmigMojJL-P6GRmT_F0eZLA1/view?usp=sharing"
+                href="https://drive.google.com/file/d/1wVEXXd1nZmigMojJL-P6GRmT_F0eZLA1/view?usp=drive_link"
                 target="_blank"
                 onClick={() => updateExpanded(false)}
               >
